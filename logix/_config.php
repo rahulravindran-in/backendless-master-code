861b79074ac9910c0480accac1a4654d456ad527
rahulravindran-in <rahulravindran.in@gmail.com>
2019-07-01 02:04:01 +0530

    Create _config.php

---
+++ logix/_config.php
@@ -0,0 +1,5 @@
+<?php
+    define('USER_ROOT', '/var/www/html/backendless_io/ceac10a0309e60457d2567b349fa872bc913dc2958b9b0a86052f8a2c0752e6e'); 
+    define('CREDIT_NAME_PREFIX', 'BC'); 
+    define('BACKENDLESS_CONSTANT', 400); 
+?>
