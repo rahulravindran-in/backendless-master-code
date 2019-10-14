c3dd5393cadfeb3c692ab9f8d3a336a0ede120c5
Reposs Name <rahulravindran.in@gmail.com>
2019-10-14 12:56:35 -0400

    Create _ext_utility.php

---
+++ plugins/oauth2/_ext_utility.php
@@ -0,0 +1,10 @@
+require_once __DIR__.'/../../../../plugins/<projectHash>/oauth2/server.php';
+
+function enable_oauth2($scopeRequired){
+   $request = OAuth2\Request::createFromGlobals();
+   $response = new OAuth2\Response();
+   if (!$server->verifyResourceRequest($request, $response, $scopeRequired)) {
+       $response->send();
+       die;
+   }
+}
