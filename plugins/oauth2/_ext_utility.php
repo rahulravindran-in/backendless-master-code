262d04813ba28397e84d59ae60c79928c4a6ec85
Reposs Name <rahulravindran.in@gmail.com>
2019-11-05 11:46:13 -0500

    Update _ext_utility.php

--- plugins/oauth2/_ext_utility.php
+++ plugins/oauth2/_ext_utility.php
@@ -3,6 +3,7 @@
 require_once __DIR__.'/../../../../plugins/<projectHash>/oauth2/server.php';
 
 function enable_oauth2($scopeRequired){
+   global $server;
    $request = OAuth2\Request::createFromGlobals();
    $response = new OAuth2\Response();
    if (!$server->verifyResourceRequest($request, $response, $scopeRequired)) {
